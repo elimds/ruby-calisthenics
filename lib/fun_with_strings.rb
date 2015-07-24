43a22af61c9c310ad0c6483ed2d5f48555fd1ae5
elimds1 <ems10@cin.ufpe.br>
2015-07-24 15:15:51 +0000

    Commit inicial

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,17 +1,39 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    str = self.downcase
+    str = str.gsub(/[^a-z]/, '')
+    str == str.reverse
   end
+  
   def count_words
-    # your code here
+    str = self.downcase
+    str = str.strip
+    str = str.gsub(/[!.,-]/,'')
+    words = str.split.reject { |w| w =~ /(\W|_|\d)/ }
+    hash_words = Hash.new
+    words.each do |word|
+      hash_words[word] = words.count(word)
+    end
+    return hash_words
   end
+  
   def anagram_groups
-    # your code here
+    words = self.split
+    groups = Array.new
+    until words.empty?
+      word = words.first
+      groups.push(words.select { |v| word.downcase.chars.sort.join.eql?(v.downcase.chars.sort.join)})
+      words.delete_if{|v| word.downcase.chars.sort.join.eql?(v.downcase.chars.sort.join)}
+    end
+    return groups
   end
+  
 end
 
 # make all the above functions available as instance methods on Strings:
-
 class String
   include FunWithStrings
 end
+
+
+
--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -4,11 +4,28 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+      rules = {"R" => {"R" => 1, "P" => 2, "S" => 1},
+        "P" => {"R" => 1, "P" => 1, "S" => 2},
+        "S" => {"R" => 2, "P" => 1, "S" => 1}}
+      
+      res = rules[player1[1]][player2[1]]
+      
+      if res.nil? || res < 1
+        raise NoSuchStrategyError.new("Strategy must be one of R,P,S")
+      end
+      
+      if (res == 1)
+        return player1
+      else
+        return player2
+      end
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    if (tournament[0][0].is_a?(String))
+      return self.winner(tournament[0], tournament[1])
+    end
+    self.winner(self.tournament_winner(tournament[0]), self.tournament_winner(tournament[1]))
   end
 
 end
